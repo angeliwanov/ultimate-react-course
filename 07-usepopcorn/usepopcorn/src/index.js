5a170d72a833d08e2ec3a609638ac7355390679c
angeliwanov <angel.iwanov@gmail.com>
2023-07-30 16:09:47 +0300

    07usePopcorn TextExpander

---
+++ 07-usepopcorn/usepopcorn/src/index.js
@@ -0,0 +1,34 @@
+import React from "react";
+// import { useState } from "react";
+import ReactDOM from "react-dom/client";
+// import "./index.css";
+// import App from "./App";
+// import StarRarting from "./StarRarting";
+import ExpandText from "./TextExpander";
+
+// function ExternalPropSetting() {
+//   const [movieRating, setMovieRating] = useState(0);
+//   return (
+//     <div>
+//       <StarRarting onSetRating={setMovieRating} color="blue" size={36} />
+//       <p>This movie was rated {movieRating} stars.</p>
+//     </div>
+//   );
+// }
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    {/* <App /> */}
+    {/* <StarRarting maxRating={10} />
+    <StarRarting
+      maxRating={5}
+      color="red"
+      size={24}
+      messages={["terrible", "bad", "okay", "good", "amazing"]}
+      defaultRating={3}
+    />
+    <ExternalPropSetting /> */}
+    <ExpandText />
+  </React.StrictMode>
+);
