19e0f16f017cb40adf4fa0565e6184447103a176
angeliwanov <angel.iwanov@gmail.com>
2023-08-19 19:03:03 +0300

    NavPage css

--- 11-worldwise/worldwise/src/App.jsx
+++ 11-worldwise/worldwise/src/App.jsx
@@ -3,14 +3,16 @@ import Homepage from "./pages/Homepage";
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
 import PageNotFound from "./pages/PageNotFound";
+import AppNav from "./components/AppNav";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Homepage />} />
-        <Route path="product" element={<Product />} />
-        <Route path="pricing" element={<Pricing />} />
+        <Route path="/product" element={<Product />} />
+        <Route path="/pricing" element={<Pricing />} />
+        <Route path="/app" element={<AppNav />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
--- 11-worldwise/worldwise/src/pages/Homepage.jsx
+++ 11-worldwise/worldwise/src/pages/Homepage.jsx
@@ -1,5 +1,16 @@
+import AppNav from "../components/AppNav";
+import PageNav from "../components/PageNav";
+import { NavLink } from "react-router-dom";
+
 function Homepage() {
-  return <div>Worldwise</div>;
+  return (
+    <div>
+      <PageNav />
+      <h1>Worldwise</h1>
+      <AppNav />
+      <NavLink to="/app">Go to the App</NavLink>
+    </div>
+  );
 }
 
 export default Homepage;
