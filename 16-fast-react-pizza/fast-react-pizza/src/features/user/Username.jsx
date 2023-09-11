732760117d061d571189a4d7ebe56d051548173c
Angel Ivanov <angel.iwanov@gmail.com>
2023-09-11 16:31:01 +0300

    responsive design

--- 16-fast-react-pizza/fast-react-pizza/src/features/user/Username.jsx
+++ 16-fast-react-pizza/fast-react-pizza/src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-  return <div className="text-sm font-semibold">Angel</div>;
+  return <div className="hidden text-sm font-semibold">Angel</div>;
 }
 
 export default Username;
