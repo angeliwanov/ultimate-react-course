4de5d51a0b6f3efa3cd1341ae9195283903975fc
angeliwanov <angel.iwanov@gmail.com>
2023-09-21 09:59:01 +0300

    supabase

--- 17-the-wild-oasis/the-wild-oasis/src/pages/Cabins.jsx
+++ 17-the-wild-oasis/the-wild-oasis/src/pages/Cabins.jsx
@@ -1,11 +1,17 @@
+import { getCabins } from "../services/apiCabins";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
+import { useEffect } from "react";
 
 function Cabins() {
+  useEffect(function () {
+    getCabins().then((data) => console.log(data));
+  }, []);
   return (
     <Row type="horizontal">
       <Heading as="h1">All cabins</Heading>
       <p>TEST</p>
+      <img src="https://zvtlsprgfsmsevnitlqu.supabase.co/storage/v1/object/public/cabin-images/cabin-001.jpg?t=2023-09-21T06%3A35%3A10.553Z" />
     </Row>
   );
 }
---
+++ 17-the-wild-oasis/the-wild-oasis/src/services/supabase.js
@@ -0,0 +1,8 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://zvtlsprgfsmsevnitlqu.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp2dGxzcHJnZnNtc2V2bml0bHF1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTUyNzI5NzgsImV4cCI6MjAxMDg0ODk3OH0.diiEWaLTOwmCVHHNTGN165_oopvQhwL6miX4__Ze4H4";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
