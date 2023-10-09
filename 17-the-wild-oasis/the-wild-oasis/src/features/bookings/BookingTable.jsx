fd9fbd4ec74ae8fd773ba6cbca2018b95117d795
Angel Ivanov <angel.iwanov@gmail.com>
2023-10-09 18:56:27 +0300

    Pagination useBooking BookingDetails

--- 17-the-wild-oasis/the-wild-oasis/src/features/bookings/BookingTable.jsx
+++ 17-the-wild-oasis/the-wild-oasis/src/features/bookings/BookingTable.jsx
@@ -7,7 +7,8 @@ import Spinner from "../../ui/Spinner";
 import Pagination from "../../ui/Pagination";
 
 function BookingTable() {
-  const { bookings, isLoading } = useBookings();
+  const { bookings, isLoading, count } = useBookings();
+
   if (isLoading) return <Spinner />;
   if (!bookings.length) return <Empty resourceName="bookings" />;
 
@@ -22,7 +23,6 @@ function BookingTable() {
           <div>Amount</div>
           <div></div>
         </Table.Header>
-
         <Table.Body
           data={bookings}
           render={(booking) => (
@@ -31,7 +31,7 @@ function BookingTable() {
         />
 
         <Table.Footer>
-          <Pagination count={45} />
+          <Pagination count={count} />
         </Table.Footer>
       </Table>
     </Menus>
