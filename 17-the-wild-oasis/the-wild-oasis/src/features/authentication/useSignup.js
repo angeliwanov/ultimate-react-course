8471bbfe9bb98269b241c3946b2ac1a85f787be8
Angel Ivanov <angel.iwanov@gmail.com>
2023-10-14 10:29:26 +0300

    Header - avatar user logout

---
+++ 17-the-wild-oasis/the-wild-oasis/src/features/authentication/useSignup.js
@@ -0,0 +1,17 @@
+import { useMutation } from "@tanstack/react-query";
+import { signup as signupApi } from "../../services/apiLogin";
+import toast from "react-hot-toast";
+
+export function useSignup() {
+  const { mutate: signup, isLoading } = useMutation({
+    mutationFn: signupApi,
+    onSuccess: (user) => {
+      console.log(user);
+      toast.success(
+        "Successfully created new user. Please verify you account."
+      );
+    },
+  });
+
+  return { signup, isLoading };
+}
