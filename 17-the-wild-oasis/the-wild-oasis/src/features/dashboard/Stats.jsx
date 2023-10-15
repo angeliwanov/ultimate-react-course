454646a213808ff5806c47646f16f3f6926d8142
Angel Ivanov <angel.iwanov@gmail.com>
2023-10-15 10:34:49 +0300

    stats chart

---
+++ 17-the-wild-oasis/the-wild-oasis/src/features/dashboard/Stats.jsx
@@ -0,0 +1,49 @@
+import { formatCurrency } from "../../utils/helpers";
+import Stat from "./Stat";
+import {
+  HiOutlineBanknotes,
+  HiOutlineBriefcase,
+  HiOutlineCalendarDays,
+  HiOutlineChartBar,
+} from "react-icons/hi2";
+
+function Stats({ bookings, confirmedStays, cabinsCount, numDays }) {
+  const numBookings = bookings.length;
+  const sales = confirmedStays.reduce((acc, cur) => acc + cur.totalPrice, 0);
+  const checkins = confirmedStays.length;
+  const occupancyRate = Math.round(
+    (confirmedStays.reduce((acc, cur) => acc + cur.numNights, 0) /
+      (numDays * cabinsCount)) *
+      100
+  );
+  return (
+    <>
+      <Stat
+        title="Bookings"
+        color="blue"
+        icon={<HiOutlineBriefcase />}
+        value={numBookings}
+      />
+      <Stat
+        title="Sales"
+        color="green"
+        icon={<HiOutlineBanknotes />}
+        value={formatCurrency(sales)}
+      />
+      <Stat
+        title="Check ins"
+        color="indigo"
+        icon={<HiOutlineCalendarDays />}
+        value={checkins}
+      />
+      <Stat
+        title="Occupancy rate"
+        color="yellow"
+        icon={<HiOutlineChartBar />}
+        value={occupancyRate + "%"}
+      />
+    </>
+  );
+}
+
+export default Stats;
